ff501cdb7e524e8d11650cf4db877484c41fabc7
timmyhadwen <timmyhadwen@gmail.com>
2017-07-23 15:45:32 +1000

    Added some mockito examples

---
+++ game/src/test/java/com/deco2800/marsinvasion/GenerateActionTest.java
@@ -0,0 +1,35 @@
+package com.deco2800.marsinvasion;
+
+import com.deco2800.marsinvasion.actions.GenerateAction;
+import com.deco2800.moos.entities.Tree;
+import com.deco2800.moos.worlds.AbstractWorld;
+import com.deco2800.moos.worlds.ExampleWorld;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class GenerateActionTest {
+	@Test
+	public void TestActionCompleted() {
+		AbstractWorld w = mock(ExampleWorld.class);
+		Tree t = new Tree(w, 0,0,0);
+		GenerateAction a = new GenerateAction(t, w);
+
+		assertEquals(a.actionProgress(), 10);
+
+		for(int i = 0; i < 10; i++) {
+			a.doAction();
+		}
+
+		// Shouldn't have been called by now
+		verify(w, times(0)).addEntity(t);
+		assertEquals(a.completed(), false);
+
+		// This should trigger the action
+		a.doAction();
+		verify(w, times(1)).addEntity(t);
+		assertEquals(a.completed(), true);
+		assertEquals(a.actionProgress(), 0);
+	}
+}
