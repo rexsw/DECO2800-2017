6ca50d433704c6acb971fc0a6f6d6efc573bb856
Nick <garner104@gmail.com>
2017-08-21 17:42:43 +1000

    Added random spacman placement and enemy group placement

--- marswars/src/main/java/com/deco2800/marswars/worlds/InitialWorld.java
+++ marswars/src/main/java/com/deco2800/marswars/worlds/InitialWorld.java
@@ -60,7 +60,37 @@ public class InitialWorld extends BaseWorld {
 		this.addEntity(new Base2(this, 10, 10, 0));
 		this.addEntity(new EnemySpacman(24, 24, 0));
 		this.addEntity(new EnemyTank(20, 20, 0));
+		this.addEnemyGroup(this.getLength()/2, this.getWidth()/2);
+		this.addRandomSpacman();
 	}
+
+	/**
+	 * Adds cluster of enemy spacman, needs to be improved
+	 */
+	private void addEnemyGroup(int x, int y){
+		if(x-1 < 0 || y-1 < 0 || y +1 >= this.getLength() || x + 1 >= this.getWidth()){
+			return;
+		}
+		this.addEntity(new EnemySpacman(x, y, 0));
+		this.addEntity(new EnemySpacman(x - 1, y, 0));
+		this.addEntity(new EnemySpacman(x, y - 1, 0));
+		this.addEntity(new EnemySpacman(x + 1, y, 0));
+		this.addEntity(new EnemySpacman(x, y+1, 0));
+		this.addEntity(new EnemySpacman(x + 1, y + 1, 0));
+		this.addEntity(new EnemySpacman(x - 1, y - 1, 0));
+		this.addEntity(new EnemySpacman(x - 1, y + 1, 0));
+		this.addEntity(new EnemySpacman(x + 1, y -1, 0));
+	}
+
+	private void addRandomSpacman(){
+		Random r = new Random();
+		System.out.println(this.getLength());
+		System.out.println(r.nextInt(this.getLength()));
+		int x = r.nextInt(this.getLength()-1);
+		int y = r.nextInt(this.getWidth()-1);
+		this.addEntity(new HeroSpacman(this, x, y, 0));
+	}
+
 
 	/**
 	 * Deselects all entities
