a51660378b342533a001ed2ca762faf527c0319a
Lewis B <lbobbermen@bigpond.com>
2017-08-24 00:24:37 +1000

    Mock networking instead

--- marswars/src/test/java/com/deco2800/marswars/ChatTest.java
+++ marswars/src/test/java/com/deco2800/marswars/ChatTest.java
@@ -1,6 +1,8 @@
 package com.deco2800.marswars;
 
 import com.deco2800.marswars.net.*;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,92 +11,105 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that mocks Kryonet's Connection class for testing
+ */
+class MockConnection extends Connection {
+	// Static counter to ensure unique id
+	private static int currentConnectionId = 0;
+	// Identification integer
+	private int id;
+	private List<Listener> listeners;
+
+	public MockConnection() {
+		this.id = currentConnectionId++;
+		this.listeners = new ArrayList<>();
+	}
+
+	public int getID() {
+		return this.id;
+	}
+
+	/**
+	 * Add a listener to this connection
+	 */
+	public void addListener(Listener listener) {
+		this.listeners.add(listener);
+	}
+
+	/**
+	 * This is used to send an object _to_ this connection
+	 */
+	public int sendTCP(Object object) {
+		for(Listener listener : this.listeners) {
+			listener.received(this, object);
+		}
+		return 0;
+	}
+}
 
 /**
  * Test chat functionality
  */
 public class ChatTest {
-	int TEST_PORT = 9876;
-
-	SpacServer server;
-	SpacClient alice;
-	SpacClient bob;
-
 	ServerConnectionManager servManager;
 	ClientConnectionManager aliceManager;
 	ClientConnectionManager bobManager;
 
+	public Connection createConnection(Listener listener) {
+		MockConnection connection = new MockConnection();
+		connection.addListener(listener);
+		return connection;
+	}
+
 	@Before
 	public void setup() {
 		servManager = new ServerConnectionManager();
-		server = new SpacServer(servManager);
-
 		aliceManager = new ClientConnectionManager();
-		alice = new SpacClient(aliceManager);
-
 		bobManager = new ClientConnectionManager();
-		bob = new SpacClient(bobManager);
-	}
-
-	@After
-	public void teardown() {
-		alice.stop();
-		bob.stop();
-		server.stop();
-	}
-
-	/**
-	 * Helper function to wait for messages. This is horrible, I hate it
-	 */
-	void sleepThread() {
-		try {
-			Thread.sleep(1000);
-		} catch(InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
-	}
-
-	/**
-	 * Helper function to connect the clients to the server
-	 */
-	void startServerAndClients() {
-		try {
-			server.bind(TEST_PORT);
-		} catch (IOException e) {
-			fail("Server failed to bind");
-		}
-
-		try {
-			alice.connect(5000, "localhost", TEST_PORT);
-		} catch (IOException e) {
-			fail("Alice failed to connect");
-		}
-
-		try {
-			bob.connect(5000, "localhost", TEST_PORT);
-		} catch(IOException e) {
-			fail("Bob failed to connect");
-		}
 	}
 
 	@Test
-	public void ConnectionTest() {
-		startServerAndClients();
+	public void BasicJoinTest() {
+		Connection alice = createConnection(aliceManager);
+		JoinLobbyAction aliceJoin = new JoinLobbyAction("Alice");
+
+		servManager.received(alice, aliceJoin);
+
+		String log = "*Alice* joined the lobby.";
+		assertEquals(servManager.getLog(), log);
+		assertEquals(aliceManager.getLog(), log);
+	}
+
+	@Test
+	public void TwoJoinTest() {
+		Connection alice = createConnection(aliceManager);
+		Connection bob = createConnection(bobManager);
 
 		JoinLobbyAction aliceJoin = new JoinLobbyAction("Alice");
-		alice.sendObject(aliceJoin);
-
 		JoinLobbyAction bobJoin = new JoinLobbyAction("Bob");
-		bob.sendObject(bobJoin);
 
-		sleepThread();
+		servManager.received(alice, aliceJoin);
+		servManager.received(bob, bobJoin);
 
 		assertEquals(
 				servManager.getLog(),
-				"*Alice* joined the lobby.\n" +
+				"*Alice* joined the lobby.\n*Bob* joined the lobby."
+		);
+		assertEquals(
+				aliceManager.getLog(),
+				"*Alice* joined the lobby.\n*Bob* joined the lobby."
+		);
+		assertEquals(
+				bobManager.getLog(),
 				"*Bob* joined the lobby."
 		);
 	}
 
 	// TODO: Test JoinLobbyAction sent twice
+	// TODO: Test JoinLobbyAction with same name
+	// TODO: Test MessageAction
 }
