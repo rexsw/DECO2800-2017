4a55f1af937b5465692fb1ff7966b2d12baa8ab1
Joseph Tran <jdtran21@hotmail.com>
2017-08-24 17:16:47 +1000

    Javadoc and commented fogOfWarManager

--- marswars/src/main/java/com/deco2800/marswars/managers/FogOfWarManager.java
+++ marswars/src/main/java/com/deco2800/marswars/managers/FogOfWarManager.java
@@ -1,34 +1,54 @@
 package com.deco2800.marswars.managers;
 import com.deco2800.marswars.util.Array2D;
 
+
 /**
  * Fog of War Manager
- * @param <T>
- * 
+ * Initializes a 2D array that contains the vision state of all coordinates
+ * 0 = unseen (black zone)
+ * 1 = seen, but not currently in sight (grayed out)
+ * 2 = In sight (normal vision)
  */
-
 public class FogOfWarManager extends Manager {
-	private Array2D<Integer> fogOfWar;		//2D array corresponding to map coordinates
-									//0 = unseen
-									//1 = explored but not in sight
-									//2 = in sight
+	private Array2D<Integer> fogOfWar;		
 	
 	
-	public FogOfWarManager(int width, int height) {
-		this.fogOfWar = new Array2D<Integer>(width, height);
-		for(int i=0; i < width; i++) {
-			for(int j=0; j < height; j++) {
+	/**
+	 * Constructs the fogOfWar array when the game starts by setting all 
+	 * elements to 0.
+	 * 
+	 * @param width the width of the map
+	 * @param length the length of the map
+	 */
+	public FogOfWarManager(int width, int length) {
+		this.fogOfWar = new Array2D<Integer>(width, length);
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < length; j++) {
 				fogOfWar.set(i, j, 0);
 			}
 		}
 	}
 	
 	
-	public void setFog(int x, int y, int s) {
+	/**
+	 * Sets coordinate to a certain sight level.
+	 * 
+	 * @param x the x coordinate
+	 * @param y the y coordinate
+	 * @param s 0, 1 or 2
+	 * @return true if set correctly and false otherwise
+	 */
+	public boolean setFog(int x, int y, int s) {
+		if (s != 0 || s != 1 || s!= 2) {
+			return false;
+		}
 		fogOfWar.set(x, y, s);
+		return true;
 	}
 	
-	
+	/**
+	 * Updates the FogOfWar after each clock tick
+	 */
 	public void updateFog() {
 		
 	}
