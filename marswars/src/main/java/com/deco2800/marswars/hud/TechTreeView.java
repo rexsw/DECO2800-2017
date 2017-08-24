42614b239517c82626917548a17312f32f173109
Bosco <bosco.bartilomo@gmail.com>
2017-08-24 17:27:41 +1000

    added a button for upgrading tech

--- marswars/src/main/java/com/deco2800/marswars/hud/TechTreeView.java
+++ marswars/src/main/java/com/deco2800/marswars/hud/TechTreeView.java
@@ -2,6 +2,14 @@ package com.deco2800.marswars.hud;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.deco2800.marswars.actions.GenerateAction;
+import com.deco2800.marswars.entities.Spacman;
+import com.deco2800.marswars.managers.GameManager;
+import com.deco2800.marswars.managers.ResourceManager;
+import com.deco2800.marswars.managers.TechnologyManager;
+import technology.Technology;
+
+import java.util.Optional;
 
 public class TechTreeView extends Dialog{
 
@@ -9,13 +17,29 @@ public class TechTreeView extends Dialog{
 		super(title, skin);		
 		{
 			text("This will display the technology tree");
+			button("Upgrade Tech", 1);
 			button("OK");
+
 		}
 	}
 	
 	@Override 
 	protected void result(final Object object){
-		
+		TechnologyManager techMan = (TechnologyManager) GameManager.get().getManager(TechnologyManager.class);
+		if (object == (Object) 1) {
+			Technology tech2 = techMan.getTech(2);
+			ResourceManager resourceManager = (ResourceManager) GameManager.get().getManager(ResourceManager.class);
+			if (!techMan.getActive().contains(tech2)) {
+				if (resourceManager.getRocks() > tech2.getCost()[0]) {
+					resourceManager.setRocks(resourceManager.getRocks() - tech2.getCost()[0]);
+					techMan.addActiveTech(tech2);
+					System.out.println("YOu just upgraded the tech");
+				}
+			} else {
+				//You already have tech 2, do something else
+				System.out.println("You already have it");
+			}
+		}
 	}
 	
 }
