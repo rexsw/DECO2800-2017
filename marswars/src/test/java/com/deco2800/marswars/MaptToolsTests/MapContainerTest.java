ef77340138148292066355dde8f726c69a3017c6
matthewijordan <matthew.ian.jordan@gmail.com>
2017-08-25 15:17:51 +1000

    added test classes for MapContainer and NoiseMap

---
+++ marswars/src/test/java/com/deco2800/marswars/MaptToolsTests/MapContainerTest.java
@@ -0,0 +1,12 @@
+package com.deco2800.marswars.MaptToolsTests;
+
+import com.deco2800.marswars.entities.AbstractEntity;
+import com.deco2800.marswars.worlds.map.tools.MapContainer;
+import com.deco2800.marswars.worlds.BaseWorld;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapContainerTest {
+
+}
---
+++ marswars/src/test/java/com/deco2800/marswars/MaptToolsTests/NoiseMapTest.java
@@ -0,0 +1,10 @@
+package com.deco2800.marswars.MaptToolsTests;
+
+import com.deco2800.marswars.worlds.map.tools.MapContainer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NoiseMapTest {
+
+}
