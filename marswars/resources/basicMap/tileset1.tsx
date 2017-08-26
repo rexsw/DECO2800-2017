6b704ad74a580ecb4b123866a1845fc2f1a5477a
Jian <another080954@gmai.com>
2017-08-26 19:59:10 +1000

    testing map background

---
+++ marswars/resources/basicMap/tileset1.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset1" tilewidth="58" tileheight="36" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="10">
+  <image width="58" height="36" source="tileset/mars006.png"/>
+ </tile>
+ <tile id="11">
+  <image width="58" height="36" source="tileset/mars007.png"/>
+ </tile>
+ <tile id="12">
+  <image width="58" height="36" source="tileset/mars008.png"/>
+ </tile>
+ <tile id="13">
+  <image width="58" height="36" source="tileset/mars009.png"/>
+ </tile>
+ <tile id="14">
+  <image width="58" height="36" source="tileset/mars010.png"/>
+ </tile>
+ <tile id="15">
+  <image width="58" height="36" source="tileset/tile001.png"/>
+ </tile>
+ <tile id="16">
+  <image width="58" height="36" source="tileset/tile002.png"/>
+ </tile>
+ <tile id="17">
+  <image width="58" height="36" source="tileset/tile003.png"/>
+ </tile>
+ <tile id="18">
+  <image width="58" height="36" source="tileset/tile004.png"/>
+ </tile>
+ <tile id="19">
+  <image width="58" height="36" source="tileset/tile005.png"/>
+ </tile>
+ <tile id="29">
+  <image width="58" height="36" source="tileset/moon001.png"/>
+ </tile>
+ <tile id="30">
+  <image width="58" height="36" source="tileset/moon002.png"/>
+ </tile>
+ <tile id="31">
+  <image width="58" height="36" source="tileset/moon003.png"/>
+ </tile>
+ <tile id="32">
+  <image width="58" height="36" source="tileset/moon004.png"/>
+ </tile>
+ <tile id="33">
+  <image width="58" height="36" source="tileset/moon005.png"/>
+ </tile>
+ <tile id="34">
+  <image width="58" height="36" source="tileset/moon006.png"/>
+ </tile>
+ <tile id="35">
+  <image width="58" height="36" source="tileset/moon007.png"/>
+ </tile>
+ <tile id="36">
+  <image width="58" height="36" source="tileset/moon008.png"/>
+ </tile>
+ <tile id="37">
+  <image width="58" height="36" source="tileset/moon009.png"/>
+ </tile>
+</tileset>
