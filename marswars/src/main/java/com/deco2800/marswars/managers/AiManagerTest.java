6ea9b3428e6018bd8ef6189a4cfbd15aeb889101
Hayden Bird <haydenpeterbird@gmail.com>
2017-08-26 18:30:12 +1000

    Reenabled AI

--- marswars/src/main/java/com/deco2800/marswars/managers/AiManagerTest.java
+++ marswars/src/main/java/com/deco2800/marswars/managers/AiManagerTest.java
@@ -27,7 +27,7 @@ public class AiManagerTest extends Manager implements TickableManager, HasTeam {
 
 @Override
 public void onTick(long l) {
-	alive = false; //TODO: Reenable
+	//alive = false; //TODO: Reenable
 	if(!alive) {
 		return;
 	}
