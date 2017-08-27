26108b8707ecfb7760028b7544fe43c6c31c70d9
bwockner <braden.wockner@uqconnect.edu.au>
2017-08-28 00:04:10 +1000

    renamed soldier sprites for testing

--- marswars/resources/UnitAssets/Soldier/renameScript.sh
+++ marswars/resources/UnitAssets/Soldier/renameScript.sh
@@ -2,8 +2,8 @@
 #renaming script for testing automated sprite selection for units
 for dir in ./*/
 do
-    cd dir
-    mv *0.png default.png
-    mv *3.png selected.png
+    cd $dir
+    mv ./*0.png default.png
+    mv ./*3.png selected.png
     cd ..
 done
