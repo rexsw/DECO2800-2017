3c72342faf37675de3264b490d33435ea0c6d7b2
Tobias Guinea <tobes03@hotmail.com>
2017-09-16 20:10:46 +1000

    implemented pause into the pause menu

--- marswars/src/main/java/com/deco2800/marswars/hud/PauseMenu.java
+++ marswars/src/main/java/com/deco2800/marswars/hud/PauseMenu.java
@@ -2,12 +2,17 @@ package com.deco2800.marswars.hud;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.deco2800.marswars.managers.GameManager;
+import com.deco2800.marswars.managers.TimeManager;
 
 /*The Pause menu that will be run by the hud every time that the game is paused.
  * Includes buttons for resuming the game, for viewing the game statistics, for changing certain game settings
  * quitting to the main menu and exiting the game
  */
 public class PauseMenu extends Dialog{
+	private TimeManager timeManager = (TimeManager)
+			GameManager.get().getManager(TimeManager.class);
+	
 	public PauseMenu(String title, Skin skin) {
 		super(title, skin);
 		
@@ -19,6 +24,7 @@ public class PauseMenu extends Dialog{
 			button("Settings", 2); //$NON-NLS-1$
 			button("Quit to Main Menu", 3); //$NON-NLS-1$
 			button("Exit Game", 4); //$NON-NLS-1$
+			timeManager.pause();
 		}	
 	}
 	
@@ -31,6 +37,8 @@ public class PauseMenu extends Dialog{
 				
 			} else if (object == (Object) 4) {
 				System.exit(0);
+			} else {
+				timeManager.unPause();
 			}
 		}
 }
