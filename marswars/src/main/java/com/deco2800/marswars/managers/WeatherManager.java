78e847f9707d3c207825387890e11adffb4e3e78
Isaac Doidge <isaacdoidge@gmail.com>
2017-09-16 16:23:32 +1000

    Retrieving most recent version of WeatherManager as it wasn't a part of the recent merge (currently broken - retreiving water class to fix)

--- marswars/src/main/java/com/deco2800/marswars/managers/WeatherManager.java
+++ marswars/src/main/java/com/deco2800/marswars/managers/WeatherManager.java
@@ -7,15 +7,20 @@ import com.deco2800.marswars.entities.buildings.BuildingEntity;
 import com.deco2800.marswars.entities.buildings.Bunker;
 import com.deco2800.marswars.entities.units.Soldier;
 import com.deco2800.marswars.util.Array2D;
+import com.deco2800.marswars.util.Point;
+import com.deco2800.marswars.worlds.BaseWorld;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class WeatherManager extends Manager {
+public class WeatherManager extends Manager implements Tickable {
     private TimeManager timeManager =
             (TimeManager) GameManager.get().getManager(TimeManager.class);
     private ArrayList<BuildingEntity> pausedBuildings;
-    boolean damaged = false;
+    private boolean damaged = false;
+    private BaseWorld world = GameManager.get().getWorld();
+
 
     /**
      * Sets the relevant weather even according to the current in game time.
@@ -27,22 +32,28 @@ public class WeatherManager extends Manager {
     }
 
     /**
-     * Sets the damage over time taken by units caught in the weather event.
-     * @param damage
+     * Sets the relevant weather even according to the current in game time.
      */
-    public void applyEffects(int damage) {
-        List<BaseEntity> entities =
-                GameManager.get().getWorld().getEntities();
-        ArrayList<int []> areaOfEffect = getAffectedTiles();
+    public boolean isRaining() {
+        return timeManager.getGameDays() % 3 == 0;
+    }
+
+    /**
+     * Sets the damage over time taken by units caught in the weather event.
+     */
+    public void applyEffects() {
+
+        ArrayList<float []> areaOfEffect = getAffectedTiles();
         // Maybe set time condition based on seconds passed to ensure dot works as intended?
         // ALSO VERY INEFFICIENT
-        for (BaseEntity e: entities) {
-            // Find entity storage for water tiles to get positions, might be able to resize from there too
-            for (int [] tile: areaOfEffect) {
+        for (float [] tile: areaOfEffect) {
+            List<BaseEntity> entities =
+                    GameManager.get().getWorld().getEntities((int) tile[0],
+                            (int) tile[1]);
+            for (BaseEntity e: entities) {
                 if (e.getPosX() == tile[0] && e.getPosY() == tile[1]) {
                     if (e instanceof Soldier) {
-                        setUnitDamage(e, damage);
-                        applyContinuousDamage(e, damage);
+                        applyContinuousDamage(e);
                     } else if (e instanceof BuildingEntity) {
                         haltProduction((BuildingEntity) e);
                     }
@@ -51,30 +62,25 @@ public class WeatherManager extends Manager {
         }
     }
 
-    // May not need this
     /**
      * Sets the damage over time taken by units caught in the weather event.
-     * @param damage
+     * @param e - the entity to take damage
      */
-    public void setUnitDamage(BaseEntity e, int damage) {
-        ((Soldier) e).setDamage(damage);
-    }
-
-    /**
-     * Sets the damage over time taken by units caught in the weather event.
-     * @param damage
-     */
-    public void applyContinuousDamage(BaseEntity e, int damage) {
+    public void applyContinuousDamage(BaseEntity e) {
         int timeBetween = 5;
         // assuming that the function will not always be called 5 seconds apart
         if (timeManager.getPlaySeconds() % 10 > timeBetween && ! damaged) {
-            ((Soldier) e).setDamage(damage);
+            ((Soldier) e).setDamage(((Soldier) e).getMaxHealth() / 5);
             damaged = true;
         } else {
             damaged = false;
         }
     }
 
+    /**
+     * Halts the production or build progression of the passed building
+     * @param e - the building to be affected
+     */
     public void haltProduction(BuildingEntity e) {
         pausedBuildings.add(e);
         if (e instanceof Barracks) {
@@ -102,39 +108,125 @@ public class WeatherManager extends Manager {
         }
     }
 
-/*    *//**
-     * Returns the tiles affected by the current weather event.
-     * @return
-     *//*
-    public Array2D<List<BaseEntity>> getTileMap(
-            Array2D<List<BaseEntity>> map) {
-        return map;
-    }*/
-
     /**
      * Returns the tiles affected by the current weather event.
      * @return
      */
-    public ArrayList<int []> getAffectedTiles() {
-        Array2D<List<BaseEntity>> tileMap =
-                GameManager.get().getWorld().getCollisionMap();
-        ArrayList<int []> affectedTiles = new ArrayList<>();
-        // VERY INEFFICIENT SEE IF CAN RECORD POSITION OF WATER ON MAP INITALISATION
-        // Iterate over x coord
+    public ArrayList<float []> getAffectedTiles() {
+        List<BaseEntity> entities =
+                GameManager.get().getWorld().getEntities();
+        ArrayList<float []> affectedTiles = new ArrayList<>();
+
+        for (BaseEntity e : entities) {
+            if (e instanceof Water) {
+                float [] coords = {e.getPosX(), e.getPosY()};
+                affectedTiles.add(coords);
+            }
+        }
+        /*// Iterate over x coord
         for (int i = 0; i < tileMap.getLength(); i++) {
             // Iterate over y coord
             for (int j = 0; j < tileMap.getWidth(); j++) {
                 // Iterate through entity list at current coord
                 for (int k = 0; k < tileMap.get(i, j).size(); k++) {
                     // NEED TO DEFINE NEW ENTITY TYPE FOR WATER (and others if implementing other effects)
-                    if (tileMap.get(i, j).get(k).getTexture() == "waterTile") {
+                    if (tileMap.get(i, j).get(k).getEntityType().equals(5)) {
                         int [] coords = {i, j};
                         affectedTiles.add(coords);
                     }
                 }
             }
-        }
+        }*/
         return affectedTiles;
     }
 
+
+
+    /**
+     * Checks that the given point is within the bounds of the map.
+     * @param p
+     * @return
+     */
+    public boolean checkPosition(Point p) {
+        /* Ensure new water position is on the map */
+        return (p.getX() < 0 || p.getY() < 0 || p.getX() > world.getWidth()
+                || p.getY() > world.getLength());
+    }
+
+    // ALTERNATELY COULD JUST INCREASE THE SIZE OF A SINGLE ENTITY?
+    /**
+     * Generates new water entities in a random position adjacent to existing
+     * water entities with a fixed chance.
+     * @param existingWater
+     */
+    public void generateWater(Water existingWater) {
+        Random random = new Random();
+        if (random.nextInt(100) < 11) {
+            Random r = new Random();
+            Point p = new Point(existingWater.getPosX() + r.nextInt(2)
+                    - 1, existingWater.getPosY() + r.nextInt(2) - 1);
+            if (! this.checkPosition(p)) {
+                generateWater(existingWater);
+            }
+			/* Check that the new position does not contain water */
+            List<BaseEntity> entities = world.getEntities((int) p.getX(),
+                    (int) p.getY());
+            boolean canPlace = true;
+            for (BaseEntity e: entities) {
+                if (e instanceof Water) {
+                    canPlace = false;
+                } else {
+                    canPlace = true;
+                }
+            }
+            if (canPlace) {
+                new Water(world, p.getX(), p.getY(), 0, 52,
+                        52);
+            } else {
+                return;
+            }
+        }
+    }
+
+    /**
+     * Generates the flooding effect. Multiplies existing water on the map
+     * when it is raining in the game, or creates new water which is then
+     * multiplied.
+     */
+    public void generateFlood() {
+        List<BaseEntity> entities = world.getEntities();
+        // Find existing water entities
+        for (BaseEntity e : entities) {
+            if (e instanceof Water) {
+                this.generateWater((Water) e);
+            }
+        }
+        // No water found in map... Create some
+        Random r = new Random();
+        Point p = new Point(world.getWidth() - r.nextInt(10)
+                - 1, world.getLength() - r.nextInt(10) - 1);
+        // Ensure position valid (should be trivially true)
+        if (checkPosition(p)) {
+            Water firstDrop = new Water(world, p.getX(),
+                    p.getY(), 0, 52, 52);
+            this.generateWater(firstDrop);
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Causes effects to come into play each game tick.
+     * @param tick Current game tick
+     */
+    @Override
+    public void onTick(int tick) {
+        if (this.isRaining()) {
+            //this.generateFlood();
+            //this.applyEffects();
+        } else {
+            this.resumeProduction();
+        }
+    }
 }
+
