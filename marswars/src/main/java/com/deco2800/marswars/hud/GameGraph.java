08a51c73a6210c6683c6a8642247149b2184c960
scott whittington <rexsw98@hotmail.com>
2017-09-20 20:39:13 +1000

    added more to the game stats graph and added more type safety

--- marswars/src/main/java/com/deco2800/marswars/hud/GameGraph.java
+++ marswars/src/main/java/com/deco2800/marswars/hud/GameGraph.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.deco2800.marswars.managers.AbstractPlayerManager;
 import com.deco2800.marswars.managers.AiManager;
+import com.deco2800.marswars.managers.ColourManager;
 import com.deco2800.marswars.managers.GameBlackBoard;
+import com.deco2800.marswars.managers.GameBlackBoard.Field;
 import com.deco2800.marswars.managers.GameManager;
 
 /**
@@ -20,10 +23,10 @@ public class GameGraph{
 	
 	private Skin skin;
 	private ShapeRenderer renderer;
-	private String Graphtype;
+	private Field Graphtype;
 	private float[] vertices;
 	
-	public GameGraph(String graphtype){
+	public GameGraph(Field graphtype){
 		renderer = new ShapeRenderer();
 		Graphtype = graphtype;
 	}
@@ -32,14 +35,16 @@ public class GameGraph{
 		//renderer.setProjectionMatrix((new OrthographicCamera(1920, 1080)).combined);
 		renderer.begin(ShapeType.Line);
 		//fix up the position
-		if(Graphtype == "Fake") {
+		if(Graphtype == null) {
 		}
 		else {
 			for(int i: ((AiManager) GameManager.get().getManager(AiManager.class)).getAiTeam()) {
 				vertices = ((GameBlackBoard) GameManager.get().getManager(GameBlackBoard.class)).getHistory(i, Graphtype);
+				renderer.setColor(((ColourManager) GameManager.get().getManager(ColourManager.class)).getLibColour(i));
 				renderer.polyline(vertices);
 			}
 			vertices = ((GameBlackBoard) GameManager.get().getManager(GameBlackBoard.class)).getHistory(-1, Graphtype);
+			renderer.setColor(((ColourManager) GameManager.get().getManager(ColourManager.class)).getLibColour(-1));
 			renderer.polyline(vertices);
 		}
 		renderer.end();
--- marswars/src/main/java/com/deco2800/marswars/hud/GameStats.java
+++ marswars/src/main/java/com/deco2800/marswars/hud/GameStats.java
@@ -26,6 +26,7 @@ import com.deco2800.marswars.managers.GameBlackBoard;
 import com.deco2800.marswars.managers.GameManager;
 import com.deco2800.marswars.managers.TextureManager;
 import com.deco2800.marswars.managers.TimeManager;
+import com.deco2800.marswars.managers.GameBlackBoard.Field;
 
 /**
  * Displays the game stats in a separate window during the 
@@ -62,7 +63,7 @@ public class GameStats{
 		this.textureManager = textureManager; 
 		this.window = new Window("SPACWARS STATS", skin); //$NON-NLS-1$
 		window.setVisible(false);
-		gameGraph = new GameGraph("Fake"); 
+		gameGraph = new GameGraph(null); 
 	}
 	
 	/**
@@ -126,43 +127,49 @@ public class GameStats{
 		bioButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Biomass");
+				gameGraph = new GameGraph(Field.BIOMASS);
 			}
 		});
 		crystalButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Crystal");
+				gameGraph = new GameGraph(Field.CRYSTAL);
 			}
 		});
 		rockButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Rocks");
+				gameGraph = new GameGraph(Field.ROCKS);
 			}
 		});
 		waterButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Water");
+				gameGraph = new GameGraph(Field.WATER);
 			}
 		});
 		b5.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Combat Units");
+				gameGraph = new GameGraph(Field.COMBAT_UNITS);
 			}
 		});
 		b6.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Units Lost");
+				gameGraph = new GameGraph(Field.UNITS_LOST);
 			}
 		});
 		b7.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor){
-				gameGraph = new GameGraph("Buildings");
+				gameGraph = new GameGraph(Field.BUILDINGS);
+			}
+		});
+		b8.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor){
+				gameGraph = new GameGraph(Field.TECHNOLOGY);
 			}
 		});	
 
