595fef29d981d03eebf7ee5473b4efd596ff2eaa
Tobias Guinea <tobes03@hotmail.com>
2017-09-21 17:28:21 +1000

    Updated implementation of the hotkeys so that if any menu is active none of the others will open

--- marswars/src/main/java/com/deco2800/marswars/hud/Hotkeys.java
+++ marswars/src/main/java/com/deco2800/marswars/hud/Hotkeys.java
@@ -49,85 +49,100 @@ public class Hotkeys {
 			}
 		}
 		
-		if(hud.getPauseCheck() == 0) {
-			//chat listener
-			if (Gdx.input.isKeyJustPressed(Input.Keys.C) && hud.getChatActiveCheck() == 0) {
-				this.messageWindow.setVisible(true);
-				this.messageToggle = true;
-				this.hud.setChatActiveCheck(1);
-			} else if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) && Gdx.input.isKeyJustPressed(Input.Keys.C)) {
-				this.messageWindow.setVisible(false);
-				this.messageToggle = false; 
-				this.hud.setChatActiveCheck(0);
-			}
+		//chat listener
+		if (Gdx.input.isKeyJustPressed(Input.Keys.C) && this.noActive()) {
+			this.messageWindow.setVisible(true);
+			this.messageToggle = true;
+			this.hud.setChatActiveCheck(1);
+		} else if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) && Gdx.input.isKeyJustPressed(Input.Keys.C)) {
+			this.messageWindow.setVisible(false);
+			this.messageToggle = false; 
+			this.hud.setChatActiveCheck(0);
 		}
-			
-		if(hud.getChatActiveCheck() == 0) {
-			//pause menu listener
-			if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-				if (hud.getPauseCheck() == 0){
-					pause = new PauseMenu("Pause Menu", skin, stage, stats, hud).show(stage);
-				} else {
-					timeManager.unPause();
-					hud.setPauseCheck(0);
-					pause.hide();
-				}
+		
+		//pause menu listener
+		if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+			if (this.noActive()){
+				pause = new PauseMenu("Pause Menu", skin, stage, stats, hud).show(stage);
+			} else if (hud.getPauseCheck() != 0){
+				timeManager.unPause();
+				hud.setPauseCheck(0);
+				pause.hide();
 			}
 		}
 		
-		if(hud.getChatActiveCheck() == 0 && hud.getExitCheck() == 0) {
-			if(Gdx.input.isKeyJustPressed(Input.Keys.Q)) {
-				if (hud.getExitCheck() == 0) {
-					hud.setExitCheck(1);
-					quit = new ExitGame("Quit Game", skin, hud, true).show(stage); //$NON-NLS-1$
-				}
-			}
-
-			//tech tree listener
-			if(Gdx.input.isKeyJustPressed(Input.Keys.T)) {
-				if(this.hud.getTechCheck() == 0) {
-					this.hud.setTechCheck(1);
-					this.techTree = new TechTreeView("TechTree", this.skin, this.hud).show(this.stage); //$NON-NLS-1$
-				} else {
-					this.hud.setTechCheck(0);
-					this.techTree.hide();
-					this.timeManager.unPause();
-				}
-			}
-			
-			//HUD toggle listener
-			if(Gdx.input.isKeyJustPressed(Input.Keys.E)) {
-
-				if (hud.isInventoryToggle()) {
-					hud.actionsWindow.setVisible(true);
-					hud.minimap.setVisible(true);
-					hud.resourceTable.setVisible(true);
-					//show (-) button to make resources invisible
-					hud.dispActions.remove();
-					hud.HUDManip.add(hud.removeActions);
-					hud.setInventoryToggle(false);
-				} else {
-					hud.actionsWindow.setVisible(false);
-					hud.minimap.setVisible(false);
-					hud.resourceTable.setVisible(false);
-					//show (+) to show resources again
-					hud.removeActions.remove();
-					hud.HUDManip.add(hud.dispActions);
-					hud.setInventoryToggle(true);
-				}
-			}
-			
-			//help button listener
-			if(Gdx.input.isKeyJustPressed(Input.Keys.H)) {
-				if (hud.getHelpCheck() == 0) {
-					hud.setHelpCheck(1);
-					help = new WorkInProgress("Help  Menu", skin, hud).show(stage); //$NON-NLS-1$
-				} else {
-					hud.setHelpCheck(0);
-					help.hide();
-					timeManager.unPause();
-				}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.Q)) {
+			if (this.noActive()) {
+				hud.setExitCheck(1);
+				quit = new ExitGame("Quit Game", skin, hud, true).show(stage); //$NON-NLS-1$
 			}
 		}
+
+		//tech tree listener
+		if(Gdx.input.isKeyJustPressed(Input.Keys.T)) {
+			if(this.noActive()) {
+				this.hud.setTechCheck(1);
+				this.techTree = new TechTreeView("TechTree", this.skin, this.hud).show(this.stage); //$NON-NLS-1$
+			} else if (hud.getTechCheck() != 0){
+				this.hud.setTechCheck(0);
+				this.techTree.hide();
+				this.timeManager.unPause();
+			}
+		}
+		
+		//HUD toggle listener
+		if(Gdx.input.isKeyJustPressed(Input.Keys.E)) {
+
+			if (hud.isInventoryToggle()) {
+				hud.actionsWindow.setVisible(true);
+				hud.minimap.setVisible(true);
+				hud.resourceTable.setVisible(true);
+				//show (-) button to make resources invisible
+				hud.dispActions.remove();
+				hud.HUDManip.add(hud.removeActions);
+				hud.setInventoryToggle(false);
+			} else {
+				hud.actionsWindow.setVisible(false);
+				hud.minimap.setVisible(false);
+				hud.resourceTable.setVisible(false);
+				//show (+) to show resources again
+				hud.removeActions.remove();
+				hud.HUDManip.add(hud.dispActions);
+				hud.setInventoryToggle(true);
+			}
+		}
+		
+		//help button listener
+		if(Gdx.input.isKeyJustPressed(Input.Keys.H)) {
+			if (this.noActive()) {
+				hud.setHelpCheck(1);
+				help = new WorkInProgress("Help  Menu", skin, hud).show(stage); //$NON-NLS-1$
+			} else if (hud.getHelpCheck() != 0) {
+				hud.setHelpCheck(0);
+				help.hide();
+				timeManager.unPause();
+			}
+		}	
+		
+			
+
+	}
+	
+	private boolean noActive() {
+		boolean retBool = false;
+		
+		if(hud.getHelpCheck() != 0) {
+			return retBool;
+		} else if (hud.getExitCheck() != 0) {
+			return retBool;
+		} else if (hud.getPauseCheck() != 0) {
+			return retBool;
+		} else if (hud.getChatActiveCheck() != 0) {
+			return retBool;
+		} else if (hud.getTechCheck() != 0) {
+			return retBool;
+		}
+		
+		return !retBool;
 	}
 }
