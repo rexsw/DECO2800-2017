dc0141e70f79dd0ce48129cf58aa041e0e521a5f
Mason <mingyu.xiong@uqconnect.edu.au>
2017-10-17 16:46:41 +1000

    try to bring jenkins back

--- marswars/src/main/java/com/deco2800/marswars/managers/TextureManager.java
+++ marswars/src/main/java/com/deco2800/marswars/managers/TextureManager.java
@@ -48,7 +48,7 @@ public class TextureManager extends Manager {
 	    	textureMap.put("redSelect4", new Texture("resources/buildSelect/redSelect4.png"));
 	        this.saveTexture("selected", "resources/placeholderassets/selected.png");
 	        this.saveTexture("selected_black", "resources/placeholderassets/selected_black.png");
-	        textureMap.put("tileSelectGreen", new Texture("resources/shopAssets/GreenSelect.png"));
+	        textureMap.put("tileSelectGreen", new Texture("resources/shopAssets/greenSelect.png"));
 	        textureMap.put("tileSelectRed", new Texture("resources/shopAssets/redSelect.png"));
         //Buildings
         	//Base Stages
