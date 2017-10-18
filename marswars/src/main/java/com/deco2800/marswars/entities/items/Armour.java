73527eef240d4b2bcd01daa298786e390f7e7946
Tobias Guinea <tobes03@hotmail.com>
2017-10-19 07:16:08 +1000

    moar code smells!!

--- marswars/src/main/java/com/deco2800/marswars/entities/items/Armour.java
+++ marswars/src/main/java/com/deco2800/marswars/entities/items/Armour.java
@@ -35,8 +35,6 @@ public class Armour extends Item {
 		this.effects = new ArrayList<>();
 		this.type = type;
 		this.level = level;
-//		this.effects.add(new DefenceEffect(getArmourValue(), getArmourHealth
-//				(), getMoveSpeed()));
 		this.effects.add(new DefenceEffect(getArmourValue(), getArmourHealth(), getMoveSpeed(), Target.SELF));
 	}
 
