21fe9125626f2558b318e1281c575c9262310f7e
Lucho Andrade <luan95_12@hotmail.com>
2017-10-18 19:38:52 +1000

    Fixed bug in game: InterruptedException reinterrupted

--- marswars/src/main/java/com/deco2800/marswars/InitiateGame/Game.java
+++ marswars/src/main/java/com/deco2800/marswars/InitiateGame/Game.java
@@ -508,7 +508,7 @@ public class Game{
 						Thread.sleep(1);
 					} catch (InterruptedException e) {
 						LOGGER.error(e.toString());
-						throw new RuntimeException(e);
+						Thread.currentThread().interrupt();
 					}
 				}
 			}
