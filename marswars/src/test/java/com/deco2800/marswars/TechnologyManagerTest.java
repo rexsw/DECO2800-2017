0d0155fac265d30192d117bc6515008ff8b2293c
Bosco <bosco.bartilomo@gmail.com>
2017-10-22 19:18:37 +1000

    Updated TechnologyManager Tester

--- marswars/src/test/java/com/deco2800/marswars/TechnologyManagerTest.java
+++ marswars/src/test/java/com/deco2800/marswars/TechnologyManagerTest.java
@@ -1,8 +1,11 @@
 package com.deco2800.marswars;
 
+import com.deco2800.marswars.managers.GameManager;
+import com.deco2800.marswars.managers.ResourceManager;
 import com.deco2800.marswars.managers.TechnologyManager;
 import com.deco2800.marswars.technology.Technology;
 import org.junit.Test;
+import org.lwjgl.Sys;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +20,10 @@ import static org.junit.Assert.*;
  * Hi Sam :)
  */
 public class TechnologyManagerTest {
+
     private TechnologyManager technologyManager = new TechnologyManager();
+    Technology tech1 = new Technology(new int[]{10, 0, 0}, "Armour 1", new ArrayList<>(),
+            "1st level Armour tech");
 
     /**
      * Checks to see if technology constructors work properly
@@ -42,7 +48,183 @@ public class TechnologyManagerTest {
         technologyManager.addActiveTech(technology);
         //should have sufficient resources
         assertTrue(technologyManager.getActive().contains(technology));
-        //technologyManager.checkPrereqs(technologyManager, technologyManager.getTech(1), 1, -1);
     }
+
+    @Test
+    public void setUpUnlockStates() throws Exception {
+    }
+
+    @Test
+    public void setUpArmourTechs() throws Exception {
+    }
+
+    @Test
+    public void setUpDamageTechs() throws Exception {
+    }
+
+    @Test
+    public void setUpSpeedTechs() throws Exception {
+    }
+
+    @Test
+    public void setUpHealthTechs() throws Exception {
+    }
+
+    @Test
+    public void setUpSpecialTechs() throws Exception {
+    }
+
+    @Test
+    public void setUpHeroFactoryTech() throws Exception {
+    }
+
+    @Test
+    public void setUpArmourItemLevelTechs() throws Exception {
+    }
+
+    @Test
+    public void setUpSpecialItemsTech() throws Exception {
+    }
+
+    @Test
+    public void setUpWeaponLevelTechs() throws Exception {
+    }
+
+    @Test
+    public void setUnitAttributes() throws Exception {
+    }
+
+    @Test
+    public void getTech() throws Exception {
+        assertTrue(technologyManager.getTech(1).equals(tech1));
+    }
+
+    @Test
+    public void getUnitAttribute() throws Exception {
+    }
+
+    @Test
+    public void getAllTechs() throws Exception {
+        assertTrue(!technologyManager.getAllTechs().equals(null));
+    }
+
+    @Test
+    public void getActive() throws Exception {
+    }
+
+    @Test
+    public void addActiveTech() throws Exception {
+    }
+
+    @Test
+    public void attackUpgrade() throws Exception {
+    }
+
+    @Test
+    public void armourUpgrade() throws Exception {
+    }
+
+    @Test
+    public void speedUpgrade() throws Exception {
+    }
+
+    @Test
+    public void healthUpgrade() throws Exception {
+    }
+
+    @Test
+    public void nootropicsUpgrade() throws Exception {
+    }
+
+    @Test
+    public void cowLevelUpgrade() throws Exception {
+    }
+
+    @Test
+    public void steroidsUpgrade() throws Exception {
+    }
+
+    @Test
+    public void vampirismUpgrade() throws Exception {
+    }
+
+    @Test
+    public void unlockArmourLevelOne() throws Exception {
+    }
+
+    @Test
+    public void unlockArmourLevelTwo() throws Exception {
+    }
+
+    @Test
+    public void unlockArmourLevelThree() throws Exception {
+    }
+
+    @Test
+    public void armourIsUnlocked() throws Exception {
+    }
+
+    @Test
+    public void unlockWeaponLevelOne() throws Exception {
+    }
+
+    @Test
+    public void unlockWeaponLevelTwo() throws Exception {
+    }
+
+    @Test
+    public void unlockWeaponLevelThree() throws Exception {
+    }
+
+    @Test
+    public void weaponIsUnlocked() throws Exception {
+    }
+
+    @Test
+    public void unlockSpecial() throws Exception {
+    }
+
+    @Test
+    public void specialIsUnlocked() throws Exception {
+    }
+
+    @Test
+    public void unlockHeroFactory() throws Exception {
+    }
+
+    @Test
+    public void checkPrereqs() throws Exception {
+        ResourceManager rs = (ResourceManager) GameManager.get().getManager(ResourceManager.class);
+        assertTrue(technologyManager.checkPrereqs(technologyManager, technologyManager.getTech(2), 2,
+                1).equals("You have not researched the required technology for this upgrade"));
+        assertTrue(technologyManager.checkPrereqs(technologyManager, tech1, 1,
+                1).equals("Insufficient Rocks"));
+        rs.setRocks(200,1);
+
+        assertTrue(technologyManager.checkPrereqs(technologyManager, technologyManager.getTech(5), 5,
+                1).equals("Insufficient Crystals"));
+
+        rs.setCrystal(10,1);
+        assertTrue(technologyManager.checkPrereqs(technologyManager, technologyManager.getTech(9), 9,
+                1).equals("Insufficient Biomass"));
+        rs.setBiomass(10,1);
+        assertTrue(technologyManager.checkPrereqs(technologyManager, tech1, 1,
+                1).equals("Activating Technology!"));
+        assertTrue(technologyManager.checkPrereqs(technologyManager, tech1, 1,
+                1).equals("You have already researched this upgrade"));
+    }
+
+    @Test
+    public void activateTech() throws Exception {
+        ResourceManager rs = new ResourceManager();
+        assertTrue(!technologyManager.getActive().contains(tech1));
+        technologyManager.activateTech(technologyManager,technologyManager.getTech(1),rs,1,1);
+        assertTrue(technologyManager.getActive().contains(tech1));
+    }
+
+    @Test
+    public void getAvailableBuildings() throws Exception {
+    }
+
 
 }
