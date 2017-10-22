aa8befa415d88a4b82cdcbc2e2f23a4f549c9c42
Zeid Ismail <zeid.ismail@gmail.com>
2017-10-22 15:46:27 +1000

    Added tests for all item levels.

--- marswars/src/test/java/com/deco2800/marswars/entities/items/ArmourTest.java
+++ marswars/src/test/java/com/deco2800/marswars/entities/items/ArmourTest.java
@@ -1,55 +1,120 @@
 package com.deco2800.marswars.entities.items;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by Nick on 22/9/17.
+ * Modified by Zeid on 22/10/17
  */
 public class ArmourTest {
+
+    Armour helmet1;
+    Armour helmet2;
+    Armour helmet3;
+    Armour goggle1;
+    Armour goggle2;
+    Armour goggle3;
+
+    @Before
+    public void setUp() {
+        helmet1 = new Armour(ArmourType.HELMET, 1);
+        helmet2 = new Armour(ArmourType.HELMET, 2);
+        helmet3 = new Armour(ArmourType.HELMET, 3);
+        goggle1 = new Armour(ArmourType.GOGGLE, 1);
+        goggle2 = new Armour(ArmourType.GOGGLE, 2);
+        goggle3 = new Armour(ArmourType.GOGGLE, 3);
+    }
+
+
     @Test
-    public void constructorTest () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
-        Assert.assertTrue(armour != null);
+    public void constructorTest () {new Armour(ArmourType.HELMET, 1);
+        Assert.assertTrue(helmet1 != null);
+        Assert.assertTrue(helmet2 != null);
+        Assert.assertTrue(helmet3 != null);
+        Assert.assertTrue(goggle1 != null);
+        Assert.assertTrue(goggle2 != null);
+        Assert.assertTrue(goggle3 != null);
+
     }
 
     @Test
     public void getArmourValue () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
-        Assert.assertTrue(armour != null);
-        Assert.assertEquals(1000, armour.getArmourValue());
+        Assert.assertTrue(helmet1 != null);
+        Assert.assertEquals(1000, helmet1.getArmourValue());
+        Assert.assertTrue(helmet2 != null);
+        Assert.assertEquals(1200, helmet2.getArmourValue());
+        Assert.assertTrue(helmet3 != null);
+        Assert.assertEquals(1400, helmet3.getArmourValue());
+        Assert.assertTrue(goggle1 != null);
+        Assert.assertEquals(3000, goggle1.getArmourValue());
+        Assert.assertTrue(goggle2 != null);
+        Assert.assertEquals(3600, goggle2.getArmourValue());
+        Assert.assertTrue(goggle3 != null);
+        Assert.assertEquals(4200, goggle3.getArmourValue());
     }
 
     @Test
     public void getArmourHealth () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
-        Assert.assertEquals(500, armour.getArmourHealth());
+        Assert.assertEquals(500, helmet1.getArmourHealth());
+        Assert.assertEquals(600, helmet2.getArmourHealth());
+        Assert.assertEquals(700, helmet3.getArmourHealth());
+        Assert.assertEquals(1500, goggle1.getArmourHealth());
+        Assert.assertEquals(1800, goggle2.getArmourHealth());
+        Assert.assertEquals(2100, goggle3.getArmourHealth());
+
     }
 
     @Test
     public void getArmourMoveSpeed () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
-        Assert.assertTrue(Math.abs(armour.getMoveSpeed() - 0.05f)< 0.00001f);
+        Assert.assertTrue(Math.abs(helmet1.getMoveSpeed() - 0.05f)< 0.00001f);
+        Assert.assertTrue(Math.abs(helmet2.getMoveSpeed() - 0.06f)< 0.00001f);
+        Assert.assertTrue(Math.abs(helmet3.getMoveSpeed() - 0.07f)< 0.00001f);
+        Assert.assertTrue(Math.abs(goggle1.getMoveSpeed() - 0.1f)< 0.00001f);
+        Assert.assertTrue(Math.abs(goggle2.getMoveSpeed() - 0.12f)< 0.00001f);
+        Assert.assertTrue(Math.abs(goggle3.getMoveSpeed() - 0.14f)< 0.00001f);
     }
+
     @Test
     public void getArmourType () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
-        Assert.assertEquals("ARMOUR", armour.getItemType().toString());
+        Assert.assertEquals("ARMOUR", helmet1.getItemType().toString());
+        Assert.assertEquals("ARMOUR", helmet2.getItemType().toString());
+        Assert.assertEquals("ARMOUR", helmet3.getItemType().toString());
+        Assert.assertEquals("ARMOUR", goggle1.getItemType().toString());
+        Assert.assertEquals("ARMOUR", goggle2.getItemType().toString());
+        Assert.assertEquals("ARMOUR", goggle3.getItemType().toString());
+    }
+
+    @Test
+    public void getName(){
+        Assert.assertEquals("Combat Helmet", helmet1.getName());
+        Assert.assertEquals("Combat Helmet", helmet2.getName());
+        Assert.assertEquals("Combat Helmet", helmet3.getName());
+        Assert.assertEquals("Tactical Goggle", goggle1.getName());
+        Assert.assertEquals("Tactical Goggle", goggle2.getName());
+        Assert.assertEquals("Tactical Goggle", goggle3.getName());
     }
 
     @Test
     public void getArmourDescription () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
+        // description for base item only, as ShopDialog.getItemStats() handles different levels
         String test = "Combat Helmet\n" +
                 "Armour: 1000\n" +
                 "Health: 500\n" +
                 "MovementSpeed: 0.05";
-        Assert.assertEquals(test, armour.getDescription());
+        Assert.assertEquals(test, helmet1.getDescription());
+        String test2 = "Tactical Goggle\n" +
+                "Armour: 3000\n" +
+                "Health: 1500\n" +
+                "MovementSpeed: 0.1";
     }
 
     @Test
     public void getArmourTexture () {
-        Armour armour = new Armour(ArmourType.HELMET, 1);
-        Assert.assertEquals("helmet_1", armour.getTexture().toString());
+        // for base items only, as ShopDialog handles displaying different item levels
+        Assert.assertEquals("helmet_1", helmet1.getTexture().toString());
+        Assert.assertEquals("goggle_1", goggle1.getTexture().toString());
+
     }
 }
--- marswars/src/test/java/com/deco2800/marswars/entities/items/WeaponTest.java
+++ marswars/src/test/java/com/deco2800/marswars/entities/items/WeaponTest.java
@@ -1,54 +1,112 @@
 package com.deco2800.marswars.entities.items;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by Nick on 22/9/17.
+ * Modified by Zeid on 22/10/17
  */
 public class WeaponTest {
+    Weapon handgun1;
+    Weapon handgun2;
+    Weapon handgun3;
+    Weapon rifle1;
+    Weapon rifle2;
+    Weapon rifle3;
+
+    @Before
+    public void setUp() {
+        handgun1 = new Weapon(WeaponType.HANDGUN, 1);
+        handgun2 = new Weapon(WeaponType.HANDGUN, 2);
+        handgun3 = new Weapon(WeaponType.HANDGUN, 3);
+        rifle1 = new Weapon(WeaponType.RIFLE, 1);
+        rifle2 = new Weapon(WeaponType.RIFLE, 2);
+        rifle3 = new Weapon(WeaponType.RIFLE, 3);
+
+    }
+
     @Test
     public void ConstructorTest(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertTrue(weapon != null);
+        Assert.assertTrue(handgun1 != null);
+        Assert.assertTrue(handgun2 != null);
+        Assert.assertTrue(handgun3 != null);
+        Assert.assertTrue(rifle1 != null);
+        Assert.assertTrue(rifle2 != null);
+        Assert.assertTrue(rifle3 != null);
+
     }
     @Test
     public void getWeaponDamage(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals(100, weapon.getWeaponDamage());
+        Assert.assertEquals(100, handgun1.getWeaponDamage());
+        Assert.assertEquals(120, handgun2.getWeaponDamage());
+        Assert.assertEquals(140, handgun3.getWeaponDamage());
+        Assert.assertEquals(300, rifle1.getWeaponDamage());
+        Assert.assertEquals(360, rifle2.getWeaponDamage());
+        Assert.assertEquals(420, rifle3.getWeaponDamage());
     }
+
     @Test
     public void getWeaponRange(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals(5, weapon.getWeaponRange());
+        Assert.assertEquals(5, handgun1.getWeaponRange());
+        Assert.assertEquals(6, handgun2.getWeaponRange());
+        Assert.assertEquals(7, handgun3.getWeaponRange());
+        Assert.assertEquals(10, rifle1.getWeaponRange());
+        Assert.assertEquals(12, rifle2.getWeaponRange());
+        Assert.assertEquals(14, rifle3.getWeaponRange());
     }
+
     @Test
     public void getWeaponSpeed(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals(20, weapon.getWeaponSpeed());
+        Assert.assertEquals(20, handgun1.getWeaponSpeed());
+        Assert.assertEquals(24, handgun2.getWeaponSpeed());
+        Assert.assertEquals(28, handgun3.getWeaponSpeed());
+        Assert.assertEquals(50, rifle1.getWeaponSpeed());
+        Assert.assertEquals(60, rifle2.getWeaponSpeed());
+        Assert.assertEquals(70, rifle3.getWeaponSpeed());
     }
+
     @Test
     public void getItemType(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals("WEAPON", weapon.getItemType().toString());
+        Assert.assertEquals("WEAPON", handgun1.getItemType().toString());
+        Assert.assertEquals("WEAPON", handgun2.getItemType().toString());
+        Assert.assertEquals("WEAPON", handgun3.getItemType().toString());
+        Assert.assertEquals("WEAPON", rifle1.getItemType().toString());
+        Assert.assertEquals("WEAPON", rifle2.getItemType().toString());
+        Assert.assertEquals("WEAPON", rifle3.getItemType().toString());
     }
+
     @Test
     public void getName(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals("Hand Gun", weapon.getName());
+        Assert.assertEquals("Hand Gun", handgun1.getName());
+        Assert.assertEquals("Hand Gun", handgun2.getName());
+        Assert.assertEquals("Hand Gun", handgun3.getName());
+        Assert.assertEquals("Rifle", rifle1.getName());
+        Assert.assertEquals("Rifle", rifle2.getName());
+        Assert.assertEquals("Rifle", rifle3.getName());
     }
+
     @Test
     public void getDescription(){
+        // description for base item only, as ShopDialog.getItemStats() handles different levels
         String test = "Hand Gun\n" +
                 "Damage: 100\n" +
                 "Speed: 20\n" +
                 "Range: 5";
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals(test, weapon.getDescription());
+        Assert.assertEquals(test, handgun1.getDescription());
+        String test2 = "Rifle\n" +
+                "Damage: 300\n" +
+                "Speed: 50\n" +
+                "Range: 10";
+        Assert.assertEquals(test2, rifle1.getDescription());
     }
+
     @Test
     public void getTexture(){
-        Weapon weapon = new Weapon(WeaponType.HANDGUN, 1);
-        Assert.assertEquals("gun_1", weapon.getTexture().toString());
+        // for base items only, as ShopDialog handles displaying different item levels
+        Assert.assertEquals("gun_1", handgun1.getTexture().toString());
+        Assert.assertEquals("rifle_1", rifle1.getTexture().toString());
+
     }
 }
